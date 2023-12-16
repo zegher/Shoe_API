f038b81b243d1bca75e66163bfb9518d109cf760
Clueless <91685310+BrendVandenEynde@users.noreply.github.com>
2023-12-16 23:17:30 +0100

    feat(ThreeJS): integrate Three.js into Vue components
    Started the Three.js project initially in Vite; however, realized it needs to be integrated into Vue. Deleted previous files and added new .vue files to set up the Three.js project within Vue on the existing ThreeJS branch.
    
    Commits included:
    1. Added on existing Three.js branch
    2. Added App.vue to act as the main Vue component
    3. Added ThreeJS.vue as a Vue component to house the Three.js logic
    4. Updated App.vue to include ThreeJS.vue
    5. Provided basic Three.js code within ThreeJS.vue
    6. Configured the project structure for seamless Vue and Three.js integration.
    
    Note: Additional commits will follow, providing step-by-step code implementation from the original Three.js project into the new Vue structure.

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<!-- src/App.vue -->
+
+<template>
+  <div id="app">
+    <div class="container">
+      <MyThreeJSComponent />
+      <div class="right-side">
+        <div class="black-div">
+          <h2>Header</h2>
+          <p>This is the right side content.</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyThreeJSComponent from './components/ThreeJS.vue';
+
+export default {
+  components: {
+    MyThreeJSComponent,
+  },
+};
+</script>
+
+<style>
+/* Add styles if needed */
+
+.container {
+  display: flex;
+  height: 100vh;
+}
+
+.right-side {
+  flex: 1;
+  background-color: #666464;
+  color: #ffffff; /* Set text color */
+  padding: 20px; /* Adjust padding as needed */
+}
+
+.right-side h2 {
+  color: #ffffff; /* Set header text color */
+}
+</style>
---
+++ src/components/ThreeJS.vue
@@ -0,0 +1,47 @@
+<!-- src/components/ThreeJS.vue -->
+
+<template>
+    <div class="left-side" ref="container"></div>
+  </template>
+  
+  <script>
+  import * as THREE from 'three';
+  
+  export default {
+    mounted() {
+      // Your Three.js code goes here
+      const scene = new THREE.Scene();
+      const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+  
+      const renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth / 2, window.innerHeight); // Set width to half of the window
+      this.$refs.container.appendChild(renderer.domElement);
+  
+      const geometry = new THREE.BoxGeometry();
+      const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      const cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
+  
+      camera.position.z = 5;
+  
+      const animate = () => {
+        requestAnimationFrame(animate);
+  
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+  
+        renderer.render(scene, camera);
+      };
+  
+      animate();
+    },
+  };
+  </script>
+  
+  <style scoped>
+  /* Add styles if needed */
+  .left-side {
+    flex: 1;
+  }
+  </style>
+  
