382588a5bf01f34e79bfef7160bb7837fb8317c9
Clueless <91685310+BrendVandenEynde@users.noreply.github.com>
2023-12-17 00:25:18 +0100

    feat: Add button and ellipses to right side
    Added a button at the bottom right of the app, triggering a console log on click. Also, included a row of ellipses with predefined colors and stroke. Adjusted their size for better visibility.

---
+++ src/App.vue
@@ -0,0 +1,82 @@
+<!-- src/App.vue -->
+
+<template>
+  <div id="app">
+    <div class="container">
+      <MyThreeJSComponent />
+      <div class="right-side">
+        <div class="black-div">
+          <h2>Laces Color</h2>
+          <!-- Button and ellipses added here -->
+            <div class="ellipses-container">
+              <div v-for="(color, index) in ellipseColors" :key="index" class="ellipse" :style="{ backgroundColor: color, borderColor: strokeColor }"></div>
+              <div class="bottom-right">
+            <button @click="handleButtonClick">Click me</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import MyThreeJSComponent from './components/ThreeJS.vue';
+
+export default {
+  components: {
+    MyThreeJSComponent,
+  },
+  data() {
+    return {
+      ellipseColors: ['#FFFFFF', '#484848', '#060606', '#FF0000', '#0000FF', '#1BBF00'],
+      strokeColor: '#64F243',
+    };
+  },
+  methods: {
+    handleButtonClick() {
+      // Add logic for button click if needed
+      console.log('Button clicked!');
+    },
+  },
+};
+</script>
+
+<style>
+/* Add styles if needed */
+
+.container {
+  display: flex;
+  height: 100vh;
+}
+
+.right-side {
+  flex: 1;
+  background-color: #141414;
+  color: #ffffff; /* Set text color */
+  padding: 20px; /* Adjust padding as needed */
+}
+
+.right-side h2 {
+  color: #ffffff; /* Set header text color */
+}
+
+.bottom-right {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  text-align: right;
+}
+
+.ellipses-container {
+  display: flex;
+  gap: 5px;
+}
+
+.ellipse {
+  width: 60px; /* Adjust width as needed */
+  height: 60px; /* Adjust height as needed */
+  border-radius: 50%;
+  border: 2px solid;
+}
+</style>
