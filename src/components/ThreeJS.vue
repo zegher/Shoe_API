f957ef517dc209e5ded352b30efbcb54794f612b
Clueless <91685310+BrendVandenEynde@users.noreply.github.com>
2023-12-19 23:46:18 +0100

    enhance ThreeJS Vue Component
    Add Shoe Parameters: Defined shoeParams to hold color information for different parts of the shoe, including inside, laces, outside_1, outside_2, sole_1, and sole_2.
    
    GUI Integration: Integrated dat.gui to create a graphical user interface for color selection. The GUI dynamically updates colors of different shoe parts based on user input.
    
    Color Visualization: Introduced shoeColors array to represent the available colors extracted from shoeParams. Updated the template to display color options in a visually appealing way.
    
    Fix GUI Setup Error: Corrected an error related to shoeParams not being defined during the GUI setup, ensuring smooth integration and color customization.
    
    Error Handling: Enhanced error handling to prevent undefined property access during rendering. Defined shoeColors and shoeParams in the component's setup function to resolve the issues.
    
    Optimization: Refactored code to ensure better readability and maintainability.

--- src/components/ThreeJS.vue
+++ src/components/ThreeJS.vue
@@ -1,6 +1,21 @@
+<!-- src/components/ThreeJS.vue -->
 <script setup>
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'; // Updated import statement
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import dat from 'dat.gui';
+
+// Define shoeParams and shoeColors
+const shoeParams = {
+  inside: { color: 0x00ff00 },
+  laces: { color: 0xff0000 },
+  outside_1: { color: 0x0000ff },
+  outside_2: { color: 0xffff00 },
+  sole_1: { color: 0x00ffff },
+  sole_2: { color: 0xff00ff },
+};
+
+const shoeColors = Object.values(shoeParams).map(param => param.color.toString(16));
 
 // Set up scene
 const scene = new THREE.Scene();
@@ -13,14 +28,50 @@ scene.background = new THREE.CubeTextureLoader().setPath("/cubemap/").load([
     "nz.png",
 ]);
 
+// Set up renderer
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+// Set up camera
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+// Add orbit controls
+const controls = new OrbitControls(camera, renderer.domElement);
+
+// Set camera position
+camera.position.z = 5;
+
+// Load 3D shoe model
+const loader = new GLTFLoader();
+let shoeModel;
+
+loader.load('models/shoe-optimized-arne.glb', (gltf) => {
+    shoeModel = gltf.scene;
+
+    shoeModel.position.set(0, 2, 0);
+
+    shoeModel.rotation.x = Math.PI / 4;
+    shoeModel.rotation.y = Math.PI / 50;
+
+    shoeModel.traverse((o) => {
+        if (o.isMesh) {
+            console.log(o.name);
+        }
+    });
+
+    scene.add(shoeModel);
+
+    // Add GUI for color selection
+    const gui = new dat.GUI();
+    for (const partName in this.shoeParams) {
+        const folder = gui.addFolder(partName);
+        folder.addColor(this.shoeParams[partName], 'color').onChange(function (value) {
+            updatePartColor(partName, value);
+        });
+        folder.open();
+    }
+});
 
 // Set up plane with rounded corners
 const roundedRectShape = new THREE.Shape();
@@ -36,6 +87,7 @@ roundedRectShape.lineTo(-2.5, -2.5 + radius);
 roundedRectShape.quadraticCurveTo(-2.5, -2.5, -2.5 + radius, -2.5);
 
 const roundedRectGeometry = new THREE.ShapeGeometry(roundedRectShape);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 }); // Define material for the plane
 const plane = new THREE.Mesh(roundedRectGeometry, material);
 plane.rotation.x = Math.PI / 2;
 plane.position.y = -1;
@@ -43,10 +95,10 @@ scene.add(plane);
 
 // Set up plane with mirror-like material
 const mirrorMaterial = new THREE.MeshStandardMaterial({
-    color: 0xffffff,  // Set color to white for reflections
-    roughness: 0.1,   // Adjust the roughness
-    metalness: 0.5,   // Adjust the metalness
-    envMap: scene.background,  // Use the scene background as the environment map for reflections
+    color: 0xffffff,
+    roughness: 0.1,
+    metalness: 0.5,
+    envMap: scene.background,
     side: THREE.DoubleSide,
 });
 
@@ -54,15 +106,6 @@ const mirrorPlane = new THREE.Mesh(roundedRectGeometry, mirrorMaterial);
 mirrorPlane.rotation.x = Math.PI / 2;
 mirrorPlane.position.y = -1;
 scene.add(mirrorPlane);
-
-// Set up camera
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
-// Add orbit controls
-const controls = new OrbitControls(camera, renderer.domElement); // Use OrbitControls directly
-
-// Set camera position
-camera.position.z = 5;
 
 // Set up raycaster and mouse vector
 const raycaster = new THREE.Raycaster();
@@ -76,49 +119,26 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight);
 
 // Add point lights
-const pointLight1 = new THREE.PointLight(0xffffff, 1); // groen - linkse lamp als je inspawnt
+const pointLight1 = new THREE.PointLight(0xffffff, 1);
 pointLight1.position.set(-2.5, 2, 0);
 scene.add(pointLight1);
 
-const pointLight2 = new THREE.PointLight(0xffffff, 1); //blauw  - rechtse lamp als je inspawnt
+const pointLight2 = new THREE.PointLight(0xffffff, 1);
 pointLight2.position.set(2.5, 2, 0);
 scene.add(pointLight2);
 
-const pointLight3 = new THREE.PointLight(0xffffff, 1); // rood  - voorste lamp als je inspawnt
+const pointLight3 = new THREE.PointLight(0xffffff, 1);
 pointLight3.position.set(0, 2, 3);
 scene.add(pointLight3);
 
-const pointLight4 = new THREE.PointLight(0xffffff, 1); // geel - verste lamp als je inspawnt
+const pointLight4 = new THREE.PointLight(0xffffff, 1);
 pointLight4.position.set(0, 2, -3);
 scene.add(pointLight4);
 
 // Add orange pointlight 5
-/*
-const pointLight5 = new THREE.PointLight(0xffffff, 1); // oranje -- onderste licht
+const pointLight5 = new THREE.PointLight(0xffffff, 1);
 pointLight5.position.set(0, -0.8, 0);
 scene.add(pointLight5);
-*/
-
-/*
-// Add pointlight helpers
-const pointLightHelper1 = new THREE.PointLightHelper(pointLight1, 1);
-scene.add(pointLightHelper1);
-
-const pointLightHelper2 = new THREE.PointLightHelper(pointLight2, 1);
-scene.add(pointLightHelper2);
-
-const pointLightHelper3 = new THREE.PointLightHelper(pointLight3, 1);
-scene.add(pointLightHelper3);
-
-const pointLightHelper4 = new THREE.PointLightHelper(pointLight4, 1);
-scene.add(pointLightHelper4);
-*/
-
-/*
-// Add pointlight 5 helper
-const pointLightHelper5 = new THREE.PointLightHelper(pointLight5, 1);
-scene.add(pointLightHelper5);
-*/
 
 function onDocumentClick(event) {
     // Calculate mouse position in normalized device coordinates
@@ -128,27 +148,27 @@ function onDocumentClick(event) {
     // Set the raycaster's ray direction and origin based on the mouse position
     raycaster.setFromCamera(mouse, camera);
 
-    // Check for intersections with the cube
-    const intersects = raycaster.intersectObject(cube);
+    // Check for intersections with the shoe model
+    const intersects = raycaster.intersectObject(shoeModel);
 
     if (intersects.length > 0) {
-        // Log the name of the clicked part to the console
-        console.log('Clicked on cube!');
+        console.log('Clicked on the shoe model!');
     }
+}
+
+// Helper function to update part color
+function updatePartColor(partName, color) {
+    shoeModel.traverse((o) => {
+        if (o.isMesh && o.name === partName) {
+            o.material.color = new THREE.Color(color);
+        }
+    });
 }
 
 // Set up animation function
 function animate() {
     requestAnimationFrame(animate);
-
-    // Rotate the cube
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    // Update controls
     controls.update();
-
-    // Render the scene
     renderer.render(scene, camera);
 }
 
@@ -157,9 +177,15 @@ animate();
 </script>
 
 <template>
-    <!-- If you have any Vue template content, you can add it here -->
+  <div class="left-side" ref="container"></div>
+  <div class="right-side">
+      <h1>Pick a color</h1>
+      <div class="color-container">
+          <div class="color" v-for="color in shoeColors" :style="{ backgroundColor: '#' + color }"></div>
+      </div>
+  </div>
 </template>
 
 <style scoped>
-    /* If you have any scoped styles, you can add them here */
+    /* If you have any CSS, you can add it here */
 </style>
